e19add77376c8469f6f1ecc7e88e724c987d09ac
kassergeymac <kassergeymac@gmail.com>
2019-06-08 22:34:38 +0300

    added json parser

---
+++ Graphics/Graphics/JSONParser/TelegramJsonParser.swift
@@ -0,0 +1,15 @@
+//
+//  TelegramJsonParser.swift
+//  Graphics
+//
+//  Created by kassergey on 6/8/19.
+//  Copyright © 2019 kassergey. All rights reserved.
+//
+
+import UIKit
+
+class TelegramJsonParser: NSObject {
+    
+    
+
+}
